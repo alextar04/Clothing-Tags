f1ed644b0b903a684cd1abe0c43c518528e370ef
alextar04 <taranlesha@mail.ru>
2020-07-12 03:50:28 +0300

    Initial Commit

---
+++ Clothing Tags/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clothing Tags
+//
+//  Created by Alexey Taran on 12.07.2020.
+//  Copyright © 2020 Alexey Taran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
